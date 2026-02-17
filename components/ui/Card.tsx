274f127a226d43f112ddf24448dec2427fe2e1c6
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-17 18:02:56 +0530

    added a card.

---
+++ components/ui/Card.tsx
@@ -0,0 +1,5 @@
+export const Card = ({ children, className = "", bgColor = "bg-white" }: { children: React.ReactNode, className?: string, bgColor?: string }) => (
+  <div className={`${bgColor} border-4 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] p-6 ${className}`}>
+    {children}
+  </div>
+);
